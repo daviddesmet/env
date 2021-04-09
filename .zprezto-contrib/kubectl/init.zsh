281245174dccd3b6a7ef271a68f7adaad6254def
David De Smet <2607383+daviddesmet@users.noreply.github.com>
2021-04-09 11:42:54 -0500

    Create init.zsh for kubectl

---
+++ .zprezto-contrib/kubectl/init.zsh
@@ -0,0 +1,25 @@
+#
+# Provides 'kubectl' aliases and utiities.
+#
+
+# Return if requirements are not found.
+if (( ! ${+commands[kubectl]} )); then
+  return 1
+fi
+
+# Enable completion for 'kubectl'.
+cache_file="${0:h}/cache.zsh"
+kubectl_command="${commands[kubectl]}"
+
+if [[ "${kubectl_command}" -nt "${cache_file}" || ! -s "${cache_file}" ]]; then
+  ${kubectl_command} completion zsh >! "${cache_file}" 2> /dev/null
+fi
+
+source "${cache_file}"
+unset cache_file kubectl_command
+
+#
+# Aliases
+#
+
+source "${0:h}/alias.zsh"
