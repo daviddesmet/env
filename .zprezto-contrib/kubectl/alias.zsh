e1a6593055a31e2718e1a66a8a8b769593dc5a51
David De Smet <2607383+daviddesmet@users.noreply.github.com>
2021-04-09 11:48:58 -0500

    Create kubectl alias.zsh

---
+++ .zprezto-contrib/kubectl/alias.zsh
@@ -0,0 +1,24 @@
+#
+# Kudos to Adrian Goins @ Rancher
+#
+
+alias kc=kubectl
+alias kd='kubectl describe'
+alias kdno='kubectl describe nodes'
+alias kdns='kubectl describe namespaces'
+alias kdpo='kubectl describe pods'
+alias kex='kubectl exec -i -t'
+alias kg='kubectl get'
+alias kga='kubectl get all'
+alias kgd='kubectl get deploy'
+alias kgi='kubectl get ingress'
+alias kgno='kubectl get nodes'
+alias kgns='kubectl get namespaces'
+alias kgow='kubectl get -o=wide'
+alias kgoy='kubectl get -o=yaml'
+alias kgpo='kubectl get pods'
+alias kgs='kubectl get service'
+alias klo='kubectl logs -f'
+alias kp='kubectl proxy'
+alias krm='kubectl delete'
+alias krmpo='kubectl delete pods'
